cd34c6586b54636b0f53bc361716b2b6885cf8c5
MuhammadAhmed-Developer <119342876+MuhammadAhmed-Developer@users.noreply.github.com>
2024-08-23 13:55:46 +0500

    add jest config

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,4 @@
-import { sum } from "./utils/sum";
-
+const { sum } = require('./utils/sum');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
--- utils/sum.js
+++ utils/sum.js
@@ -1,5 +1,5 @@
-// sum.js
-export function sum(a, b) {
+function sum(a, b) {
     return a + b;
 }
 
+module.exports = { sum };
